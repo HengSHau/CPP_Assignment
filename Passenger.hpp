6cf394cd4d0824815179dd68838db58ab80c5801
HengSiaHau <hengsiahau0812@hotmail.com>
2026-02-01 15:54:17 +0800

    passenger hpp

---
+++ Passenger.hpp
@@ -0,0 +1,18 @@
+#ifndef PASSENGER_HPP
+#define PASSENGER_HPP
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// This struct is the "payload" that holds the actual data
+struct Passenger {
+    string passengerID;
+    string name;
+    string seatRow;    // Keeping as string to handle inputs easily
+    string seatColumn; // Keeping as string or char
+    string pClass;     // "First", "Business", "Economy"
+};
+
+#endif
